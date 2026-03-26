b6b721b33ffcded1ef02f43ff315ddec17254bbd
dinesh_v <dinesh.varadhan10@gmail.com>
2026-03-26 20:53:47 +0530

    feat(user-parts):UI Enhanecment - handled broken images with fallback (no-image-available) - updated angular.json to serve assets correctly - refined responsive UI for feature parts grid

--- src/app/features/user/user-dashboard/user-part-list/user-part-list.component.ts
+++ src/app/features/user/user-dashboard/user-part-list/user-part-list.component.ts
@@ -24,10 +24,19 @@ constructor(private partService: PartService,
     this.partService.getAllParts().subscribe({
       next:(res)=>{
         this.parts = res.data?? [];//if data exists → use it.undefined fallback to empty array
+     this.parts[0].imageUrl = 'assets/shared/fake-image.png';
       },
       error(err){
       console.error('Failed to load parts',err)
     }
     });
+  }
+  OnImageError(event:any,part: any){
+    console.log("error firing")
+    const img = event.target;
+    //prevent infinite Loop
+    if (part.imageUrl === 'assets/no-image-available.png') return;
+
+  part.imageUrl = 'assets/no-image-available.png';
   }
 }
