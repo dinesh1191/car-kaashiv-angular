0a25010dab89e686ce3e7dfeaee23d7120077771
dinesh_v <dinesh.varadhan10@gmail.com>
2026-04-10 19:39:11 +0530

    fix(auth): enforce valid phone or corporate email login validation and removed unused code

--- src/app/features/auth/login.component.ts
+++ src/app/features/auth/login.component.ts
@@ -1,13 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { ActivatedRoute, Router, RouterLink } from '@angular/router';
-
-import { MATERIAL_IMPORTS } from '../../shared/material';
-import { AuthService } from '../../core/services/auth.service';
-import { LoaderService } from '../../core/services/loader.service';
+import { FormBuilder, FormGroup, AbstractControl, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import {  Router } from '@angular/router';
 import { SnackbarService } from '../../core/services/snackbar.service';
-import { PRIME_IMPORTS } from '../../shared/prime';
 import { SharedModule } from '../../shared/shared.module';
 import { AuthFacade } from '../../core/services/auth.facade';
 
@@ -31,33 +26,68 @@ export class LoginComponent {
     private fb: FormBuilder,
     private router: Router,
     private snackbarService: SnackbarService,
-    private authFacade:AuthFacade
+    private authFacade: AuthFacade,
   ) {}
 
   ngOnInit() {
     this.loginForm = this.fb.group({
-      username: ['', Validators.required],
+      username: ['', [Validators.required, this.usernameValidator()]],
       password: ['', Validators.required],
     });
   }
-  login(credentials: any) {
-     this.authFacade.login(credentials)
+
+  usernameValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const value = control.value;
+      if (!value) return null; // required handles empty case
+
+      // If input contains @, treat as email validation
+      if (value.includes('@')) {
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@kaashiv\.com$/;
+
+        if (!emailRegex.test(value)) {
+          return { invalidCorporateEmail: true };
+        }
+        return null;
+      }
+      // Otherwise, treat as phone number validation
+      if (!/^\d+$/.test(value)) {
+        return { invalidPhoneNumber: true };
+      }
+      if (value.length !== 10) {
+        return { invalidPhoneLength: true };
+      }
+
+      if (!/^[06789]/.test(value)) {
+        return { invalidPhoneStart: true };
+      }
+      return null;
+    };
   }
 
+  login(credentials: any) {
+    this.authFacade.login(credentials);
+  }
 
   onSubmit() {
     this.submitted = true;
-    if (this.loginForm.invalid) return;   
+    if (this.loginForm.invalid) {
+      this.snackbarService.show(
+        'Please fill in all required fields correctly',
+        'error',
+      );
+      return;
+    }
 
     this.authFacade.login(this.loginForm.value).subscribe({
       next: (res) => {
-        this.snackbarService.show(res.messsage,'success');     
+        this.snackbarService.show(res.messsage, 'success');
       },
       error: (err) => {
         this.snackbarService.show(
           err?.error.message || 'Something went wrong Try again later',
           'error',
-        );        
+        );
       },
     });
   }
