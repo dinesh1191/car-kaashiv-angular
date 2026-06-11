ef67c5a0317eaeedce942c96c63cc343b3867dd7
dinesh_v <dinesh.varadhan10@gmail.com>
2026-06-11 15:04:19 +0530

    chore(auth) updated api enpoint

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -78,8 +78,7 @@ export class AuthService {
   /** Fetch current user profile from backend */
   getUserProfile(): Observable<any> {
     return this.http
-      // .get<any>(`${this.apiUrl}/api/auth/me`, {
-      .get<any>(`/auth/me`, {
+       .get<any>(`${this.apiUrl}/api/auth/me`, {     
         context: new HttpContext().set(SKIP_LOADER, true),
         withCredentials: true,     
       })  
